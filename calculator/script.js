bf92831baf464d8ffc4217fd13f5b9631c7b339d
mrinex <mrinex@mail.ru>
2020-09-26 23:06:47 +0300

    refactor: script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,12 +1,9 @@
 function expressionCalculator(expr) {
-
-  let exprNoSpace = expr.replace(/\s/g,'');
-  let regex = /(?<![0-9])\-[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+|(?<![0-9])\-[0-9]+|\W/g;
   let regexNum = /^-\d*\.?\d+$|[0-9]+/;
   let left = 0; let right = 0;
   let a = "TypeError: Division by zero.";
   let b = "ExpressionError: Brackets must be paired";
-  let array = exprNoSpace.match(regex); console.log(array);
+  let array = expr.split(' ');
   for(let i = 0; i < array.length; i++)
   {
     if(array[i] === '('){left += 1;}
@@ -33,8 +30,9 @@ function expressionCalculator(expr) {
 
       if(regexNum.test(array[i-1]) && /\//.test(array[i]) && regexNum.test(array[i+1]) && array[i+3] !== '(' && array[i-3] !== ')')
       {
-        if(Number(array[i+1]) === 0) { 
-          throw new TypeError(a);
+        if(Number(array[i+1]) === 0) {
+          array = Infinity;
+          break;
         }
         let answer = Number(array[i-1]) / Number(array[i+1]);
         array[i] = answer;//array[i-1] = ''; array[i+1] = '';
@@ -62,10 +60,31 @@ function expressionCalculator(expr) {
   return array = Number(array);
 }
 
+function count(expr) {
+  let array = expr.split(' ');
+  console.log(array);
+  let newArray = array.filter((n) => {
+    return /\W/.test(n);
+  });
+  let [ h ] = newArray;
+  switch(h) {
+    case '/':
+      return +array[0] / +array[2];
+    case '*':
+      return +array[0] * +array[2];
+    case '-':
+      return +array[0] - +array[2];
+    case '+':
+      return +array[0] + +array[2];
+    default:
+      return 0;
+  }
+}
+
 const previousOutput = document.querySelector('[data-previous-output]');
 const currentOutput = document.querySelector('[data-current-output]');
 
-document.addEventListener('click', ({target}) => {
+document.addEventListener('click', ({ target }) => {
   if (target.hasAttribute('data-number')) {
     currentOutput.innerHTML += target.innerHTML;
   }
@@ -81,8 +100,13 @@ document.addEventListener('click', ({target}) => {
   }
   if (target.hasAttribute('data-equals')) {
     if (/[0-9]+/.test(currentOutput.innerHTML)) {
-      previousOutput.innerHTML += ` ${currentOutput.innerHTML}`;
-      currentOutput.innerHTML = expressionCalculator(previousOutput.innerHTML);
+      previousOutput.innerHTML += `${currentOutput.innerHTML}`;
+      let result = expressionCalculator(previousOutput.innerHTML);
+      if (/\./.test(result)) {
+        currentOutput.innerHTML = +result.toFixed(10);
+      } else {
+        currentOutput.innerHTML = +result;
+      }
       previousOutput.innerHTML = '';
     }
   }
